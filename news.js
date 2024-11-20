e013c9ba8ad7d655d4fe1df3a0f0686fae19f6a4
Natnaree40 <natnaree40@gmail.com>
2024-11-20 16:42:05 +0800

    2nd commit: add button1

--- news.js
+++ news.js
@@ -1,16 +1,28 @@
+const button1 = document.querySelector(".vocab-title")
+const vocab = document.querySelector(".vocab-list")
+button1.addEventListener("click", function() {
+    if(vocab.style.display === 'none') {
+        vocab.style.display = 'block'
+        button1.textContent = 'ซ่อนคำศัพท์สำคัญ'
+    } else {
+        vocab.style.display = 'none'
+        button1.textContent = 'แสดงคำศัพท์สำคัญ'
+    }
+})
+
 //เลือกปุ่มและ div ที่จะซ่อน
-const button = document.querySelector(".show-translate")
+const button2 = document.querySelector(".show-translate")
 const translation = document.querySelector(".thai-translate")
 //เพิ่ม Event Listener ให้กับปุ่ม
-button.addEventListener("click", function() {
+button2.addEventListener("click", function() {
     //เช็คว่า div ซ่อนอยู่หรือไม่
     if(translation.style.display === 'none') {
         //หากซ่อนอยู่ ให้แสดง div
         translation.style.display = 'block'
-        button.textContent = 'ซ่อนคำแปล' //เปลี่ยนข้อความบนปุ่ม
+        button2.textContent = 'ซ่อนคำแปล' //เปลี่ยนข้อความบนปุ่ม
     } else {
         //หากแสดงอยู่ ให้ซ่อน div
         translation.style.display = 'none';
-        button.textContent = 'แสดงคำแปล'; //เปลี่ยนข้อความบนปุ่ม
+        button2.textContent = 'แสดงคำแปล'; //เปลี่ยนข้อความบนปุ่ม
     }
 })
